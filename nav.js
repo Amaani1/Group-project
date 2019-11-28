1de8940b7c8110e0988f65629be9463e8aff786e
NyahAnderson <nkarpaiya@yahoo.co.uk>
2019-11-28 16:20:07 +0000

    Fixed JavaScript

--- nav.js
+++ nav.js
@@ -1,23 +1,29 @@
-function validation(){
+function validation() {
+
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
   var error_message = document.getElementById("error_message");
+
   var text;
 
-  error_message.style.padding = "10px";
 
-  if(email.indexOf("@")== -1 || email.length < 6){
-    text = "Please Enter A Valid Email";
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please Enter valid Email";
+    error_message.style.padding = "10px";
     error_message.innerHTML = text;
     return false;
   }
 
-  if(message.length <= 75){
+  if (message.length <= 75) {
     text = "Please Enter More Than 75 Characters";
+    error_message.style.padding = "10px";
     error_message.innerHTML = text;
     return false;
   }
-alert("Form Submitted Successfully!");
-reset();
+
+  alert("Form Submitted Successfully!");
+  document.getElementById('myform').reset();
+  error_message.style.padding = "0px";
+  error_message.innerHTML = "";
   return true;
 }
